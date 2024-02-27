7945dbc2a2d1690756a78f4607ca3527373d5750
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-02-27 16:57:51 +0100

    Most sale query added

--- app/models/cart_historic.rb
+++ app/models/cart_historic.rb
@@ -27,4 +27,33 @@ class CartHistoric < ApplicationRecord
             
   end
 
+  def self.find_hundred_items_with_quantity
+    items = find_hundred_items
+    tamanho = find_hundred_items.size - 1
+    
+    while tamanho >= 0
+      item_id = items[tamanho].item_id
+      quantity = find_sum_of_an_item(item_id)
+      
+      most_sale = MostSale.new 
+
+      item = Item.new 
+      item.id = item_id
+
+      most_sale.item = item
+      most_sale.quantity = quantity
+      most_sale.save
+      tamanho -=1
+    end
+
+  end
+
+  private
+   scope :find_hundred_items, -> { select(:item_id).distinct.limit(100) }
+   
+   scope :find_sum_of_an_item, ->(item_id) { 
+    select(:item_id).distinct
+                    .where(item_id: item_id)
+                    .sum(:quantity)
+  }
 end
